b4eaf0eaf5dd3fbf9e112410d2f9028c5acf9653
ydvaman <2017pec5250@mnit.ac.in>
2019-10-25 11:48:35 +0530

    checked

---
+++ DevOps/DevOpsSolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // My Test File for Github data upload
+
+            // The process of complete Q3 is in doc file in solution
+
+            Console.WriteLine("Hello My Nmae is Aman");
+            Console.ReadKey();
+        }
+    }
+}
